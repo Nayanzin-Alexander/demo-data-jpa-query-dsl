0bea05a85a28645dc9a2b225cdf8771133cf80d7
fin <nayanzin.alexander@gmail.com>
2019-02-23 09:54:55 +0200

    Initial commit

---
+++ src/main/java/com/nayanzin/demodatajpaquerydsl/DemoDataJpaQueryDslApplication.java
@@ -0,0 +1,13 @@
+package com.nayanzin.demodatajpaquerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDataJpaQueryDslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDataJpaQueryDslApplication.class, args);
+    }
+
+}
